e276d123ad5140ddff66e31b692c8bad977c7b31
mcsande <mcsand@fastmail.com>
2023-04-23 03:13:32 +0200

    deat layout

---
+++ config.h
@@ -0,0 +1,9 @@
+#pragma once
+#define COMBO_COUNT 1
+// #define UNICODE_SELECTED_MODES UNICODE_MODE_WINCOMPOSE
+#define UNICODE_SELECTED_MODES UNICODE_MODE_LINUX
+
+/* The way how "handedness" is decided (which half is which),
+see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
+for more options.
+*/
